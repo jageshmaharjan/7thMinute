b44e91d01b17f7c77726948f0013fad926aa5395
jugs <jageshmaharjan@hotmail.com>
2021-01-25 21:48:38 +0800

    pytorch device

--- sentiment_analyzer/api.py
+++ sentiment_analyzer/api.py
@@ -3,8 +3,8 @@ from typing import Dict, Any
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 
-from .classifier.model import Model, get_sentiment_model
-#from .entity_recognition.model import Model, get_er_model
+# from .classifier.model import Model, get_sentiment_model
+from .entity_recognition.model import Model, get_er_model
 
 app = FastAPI()
 
@@ -28,21 +28,21 @@ class EntityRecognitionResponse(BaseModel):
     entity: str
 
 
-@app.post("/sentiment", response_model=SentimentResponse)
-async def sentiment(request: SentimentRequest, model: Model = Depends(get_sentiment_model)):
-    sentiment, confidence, probabilities = model.predict(request.text)
-    return SentimentResponse(
-        sentiment=sentiment, confidence=confidence, probabilities=probabilities
-    )
-
-
-@app.post("/entity_recognition")
-async def entity_recognition(request: EntityRequest):  #, model: Model = Depends(get_er_model)
-    return "Entity Recognition Coming soon..."
-#     tokens, entity = model.predict(request.text)
-#     return EntityRecognitionResponse(
-#         tokens= tokens, entity = entity
+# @app.post("/sentiment", response_model=SentimentResponse)
+# async def sentiment(request: SentimentRequest, model: Model = Depends(get_sentiment_model)):
+#     sentiment, confidence, probabilities = model.predict(request.text)
+#     return SentimentResponse(
+#         sentiment=sentiment, confidence=confidence, probabilities=probabilities
 #     )
+
+
+@app.post("/entity_recognition", response_model=EntityRecognitionResponse)
+async def entity_recognition(request: EntityRequest, model: Model = Depends(get_er_model)):  #, model: Model = Depends(get_er_model)
+    # return "Entity Recognition Coming soon..."
+    tokens, entity = model.predict(request.text)
+    return EntityRecognitionResponse(
+        tokens= tokens, entity = entity
+    )
 
 
 @app.get("/")
